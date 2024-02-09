faf49701a0b49bd7649fa21c8c472f944092d560
amandapazetti <amandapazettiperes@gmail.com>
2024-02-09 12:00:58 -0300

    "Obtendo post por id "

---
+++ src/main/java/com/amandaramos/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.amandaramos.workshopmongo.services;
+
+import com.amandaramos.workshopmongo.dominio.Post;
+import com.amandaramos.workshopmongo.repository.PostRepository;
+import com.amandaramos.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> user = repo.findById(id);
+        if (user == null) {
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+        return user.get();
+    }
+
+
+}

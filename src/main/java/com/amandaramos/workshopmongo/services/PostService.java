c59fbe613134a69fa2c311f705aab36f8f916086
amandapazetti <amandapazettiperes@gmail.com>
2024-02-11 19:00:53 -0300

    "Consulta com varios criterios"

--- src/main/java/com/amandaramos/workshopmongo/services/PostService.java
+++ src/main/java/com/amandaramos/workshopmongo/services/PostService.java
@@ -6,8 +6,11 @@ import com.amandaramos.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+
+import static java.awt.SystemColor.text;
 
 @Service
 public class PostService {
@@ -17,15 +20,15 @@ public class PostService {
 
     public Post findById(String id) {
         Optional<Post> user = repo.findById(id);
-        if (user == null) {
-            throw new ObjectNotFoundException("Objeto não encontrado");
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+        public List<Post> findByTitle (String text){
+            return repo.searchTitle(text);
         }
-        return user.get();
+
+        public List<Post> fullSearch (String text, Date minDate, Date maxDate){
+            maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+            return repo.fullSearch(text, minDate, maxDate);
+        }
     }
 
-    public List<Post> findByTitle(String text) {
-        return repo.searchTitle(text);
-    }
-
-
-}
